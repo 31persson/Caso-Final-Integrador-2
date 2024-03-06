a884632d1d53571e361ddd8e233e740aa24c3186
31persson <jperscas@myuax.com>
2024-03-06 11:06:54 +0100

    Recurso Alimento

---
+++ RecursoAlimento.java
@@ -0,0 +1,9 @@
+public class RecursoAlimento extends Recurso {
+    // Puedes agregar atributos específicos para alimentos
+
+    public RecursoAlimento(String nombre, int cantidad, String proveedor) {
+        super(nombre, cantidad, proveedor);
+    }
+
+    // Puedes agregar métodos específicos para alimentos
+}
