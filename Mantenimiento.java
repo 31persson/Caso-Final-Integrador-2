67f2cccb99219129527d1d98ffdeccb297ea1245
31persson <jperscas@myuax.com>
2024-03-06 10:59:55 +0100

    Mantenimiento

---
+++ Mantenimiento.java
@@ -0,0 +1,11 @@
+public class Mantenimiento {
+    public void programarMantenimiento(Habitat habitat) {
+        System.out.println("Programando mantenimiento para el hábitat: " + habitat.getTipo());
+        // Implementa la lógica para programar mantenimientos
+    }
+
+    public void realizarMantenimiento(Habitat habitat) {
+        System.out.println("Realizando mantenimiento para el hábitat: " + habitat.getTipo());
+        // Implementa la lógica para realizar mantenimientos
+    }
+}
