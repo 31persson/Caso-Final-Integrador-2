8364b9308b1b988d6ccb1b1c409db34c4dd74e1a
31persson <jperscas@myuax.com>
2024-03-06 11:31:28 +0100

    Animal

--- Animal.java
+++ Animal.java
@@ -2,11 +2,13 @@ public abstract class Animal {
     protected String nombre;
     protected int edad;
     protected int salud;
+    protected Habitat habitat;
 
-    public Animal(String nombre, int edad) {
+    public Animal(String nombre, int edad, Habitat habitat) {
         this.nombre = nombre;
         this.edad = edad;
         this.salud = 100;
+        this.habitat = habitat;
     }
 
     public String getNombre() {
@@ -15,4 +17,9 @@ public abstract class Animal {
 
     public abstract void alimentar();
     public abstract void verificarSalud();
+
+    public void cambiarHabitat(Habitat nuevoHabitat) {
+        this.habitat = nuevoHabitat;
+        System.out.println(nombre + " se ha movido al hábitat " + nuevoHabitat.getTipo());
+    }
 }
