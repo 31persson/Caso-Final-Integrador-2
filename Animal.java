d99717334e5bf7712902ccb498bf69e154acf27b
31persson <jperscas@myuax.com>
2024-03-06 10:51:39 +0100

    Animal

---
+++ Animal.java
@@ -0,0 +1,14 @@
+public abstract class Animal {
+    protected String nombre;
+    protected int edad;
+    protected int salud;
+
+    public Animal(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.salud = 100;
+    }
+
+    public abstract void alimentar();
+    public abstract void verificarSalud();
+}
