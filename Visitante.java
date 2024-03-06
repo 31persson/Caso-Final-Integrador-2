a184566fb2d3aeea6ac1bb4375917569a8c39517
31persson <jperscas@myuax.com>
2024-03-06 10:54:58 +0100

    Visitante

---
+++ Visitante.java
@@ -0,0 +1,11 @@
+public class Visitante {
+    private String nombre;
+
+    public Visitante(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
