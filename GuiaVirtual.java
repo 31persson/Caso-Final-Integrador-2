20d851f9473cab0fc92a24767fca3ace9213308e
31persson <jperscas@myuax.com>
2024-03-06 10:55:33 +0100

    Guía Virtual

---
+++ GuiaVirtual.java
@@ -0,0 +1,6 @@
+public class GuiaVirtual {
+    public void proporcionarTour(Visitante visitante) {
+        System.out.println("Proporcionando tour personalizado para " + visitante.getNombre());
+        // Implementa la lógica para proporcionar tours personalizados
+    }
+}
