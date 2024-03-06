6661718d3d5284900783f028bb4de3fc201d3a76
31persson <jperscas@myuax.com>
2024-03-06 10:57:48 +0100

    Quiosco Interactivo

---
+++ QuioscoInteractivo.java
@@ -0,0 +1,6 @@
+public class QuioscoInteractivo {
+    public void mostrarInformacion(Animal animal) {
+        System.out.println("Mostrando información sobre " + animal.getNombre());
+        // Implementa la lógica para mostrar información sobre animales y hábitats
+    }
+}
